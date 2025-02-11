4ffae5e4df81a6e1346283e3ec76d642bf919c80
Alex Mwotil <mwotila@gmail.com>
2025-02-11 09:11:51 +0000

    + evaluation - Africa

--- yonga/run_ocas_wrk_tests.sh
+++ yonga/run_ocas_wrk_tests.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-for i in {1..13}
-do
-  echo "Iteration #$i: Running 'ocas' command"
-  ./target/debug/ocas -m ../docker-compose.yaml -p binpack -c ../evaluation/config.yaml -u http://127.0.0.1:32000 -s hotelreservation
-  
-  echo "Waiting for 1 minute before running 'wrk' tests..."
+LOGFILE="ocas_wrk_results.log"
+echo "Starting test run at $(date)" > "$LOGFILE"
+
+for i in $(seq 1 6); do
+  echo "Iteration #$i: Running 'ocas' command" | tee -a "$LOGFILE"
+  ./target/debug/ocas -m ../docker-compose.yaml -p spread -c ../evaluation/config.yaml -u http://127.0.0.1:32000 -s hotelreservation | tee -a "$LOGFILE"
+
+  echo "Waiting for 1 minute before running 'wrk' tests..." | tee -a "$LOGFILE"
   sleep 60
-  
-  echo "Running 5 'wrk' tests"
-  for j in {1..5}
-  do
-    echo "  Test #$j"
-    ../wrk -t 1 -c 10 -d 30 -s ../scripts/hotel-reservation/mixed-workload_type_1.lua http://cr-lsk.cranecloud.africa:5000 -R 1000000 -L | grep 'Requests/sec'
+
+  echo "Running 5 'wrk' tests" | tee -a "$LOGFILE"
+  for j in $(seq 1 5); do
+    echo "  Test #$j" | tee -a "$LOGFILE"
+    ../wrk -t 1 -c 10 -d 30 -s ../scripts/hotel-reservation/mixed-workload_type_1.lua http://cr-lsk.cranecloud.africa:5000 -R 1000000 -L | grep 'Requests/sec' | tee -a "$LOGFILE"
   done
+
+  echo "All tests completed for iteration #$i!" | tee -a "$LOGFILE"
 done
 
-echo "All tests completed!"
+echo "Test run completed at $(date)" | tee -a "$LOGFILE"
